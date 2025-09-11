6f0cd09e298a24cdf7026ff12b4b2a17e1be4fd8
Harsh Vardhan <111harshrai@gmail.com>
2025-09-11 18:15:30 +0530

    today's commit

--- app.js
+++ app.js
@@ -73,15 +73,26 @@
 
 // lis.appendChild(li);
 
+// TASK 8
 
-let img = document.createElement("img");
+// let img = document.createElement("img");
 
-img.setAttribute("src", "https://images.unsplash.com/photo-1757323149137-99e56fa4bfea?w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D");
+// img.setAttribute("src", "https://images.unsplash.com/photo-1757323149137-99e56fa4bfea?w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D");
 
-let div = document.querySelector("div");
+// let div = document.querySelector("div");
 
-div.prepend(img);
+// div.prepend(img);
 
+//  TASK-10
+
+
+let sel = document.querySelector("select");
+
+let device =  document.querySelector("#device");
+
+sel.addEventListener("change", function(dets){
+  device.textContent = `${dets.target.value}` + " device selected";
+});
 
 
 
