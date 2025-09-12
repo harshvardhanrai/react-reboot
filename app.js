94d31276874c510922188e3b7954af1d4da22f63
Harsh Vardhan <111harshrai@gmail.com>
2025-09-12 18:38:06 +0530

    form and cards

--- app.js
+++ app.js
@@ -85,14 +85,51 @@
 
 //  TASK-10
 
+let form = document.querySelector("form");
 
-let sel = document.querySelector("select");
+let inputs = document.querySelectorAll("input");
 
-let device =  document.querySelector("#device");
+let main = document.querySelector("#main");
 
-sel.addEventListener("change", function(dets){
-  device.textContent = `${dets.target.value}` + " device selected";
-});
+form.addEventListener("submit", function(dets){
+  dets.preventDefault();
+
+  let card = document.createElement("div");
+
+    card.classList.add("card");
+
+    let profile =  document.createElement("div");
+
+      profile.classList.add("profile");
+
+      let img = document.createElement("img");
+
+      img.setAttribute("src", inputs[0].value);
+
+      let h3 = document.createElement("h3");
+      h3.textContent = inputs[1].value;
+      let h5 = document.createElement("h5");
+      h5.textContent = inputs[2].value;
+      let p = document.createElement("p");
+      p.textContent = inputs[3].value;
+
+      profile.appendChild(img);
+      card.appendChild(profile);
+
+      card.appendChild(h3);
+      card.appendChild(h5);
+      card.appendChild(p);
+
+      main.appendChild(card);
+
+      inputs.forEach(function(inp){
+        if(inp.type !== "submit"){
+          inp.value = "";
+        }
+        
+      })
+
+})
 
 
 
